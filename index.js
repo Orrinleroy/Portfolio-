93fa8899703e891b2ceb0ce28fa888cedb9b3439
Edrickasper <edrickasper66@gmail.com>
2025-01-12 16:15:57 +0530

    Enhanced animations

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const spinner = document.querySelector('.spinner');
+const heroContainer = document.querySelector('.container');
 const skillsContainer = document.querySelector('.skills-container');
 const expCard = document.querySelectorAll('.timeline-card');
 const educationCard = document.querySelectorAll('.education-card');
 
 window.addEventListener('load', () => {
   spinner.classList.add('hidden');
+  heroContainer.classList.remove('hidden');
 });
 document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
@@ -14,56 +16,26 @@ document.getElementById('contactForm').addEventListener('submit', function (e) {
 });
 
 // Skill card animation
-const revealSkills = function (entries, observer) {
+const revealSections = function (entries, observer) {
   const [entry] = entries;
 
   if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove('skills-fadein');
+  entry.target.classList.remove('fadein');
   observer.unobserve(entry.target);
 };
 
-const skillsObserver = new IntersectionObserver(revealSkills, {
-  root: null,
-  threshold: 0.15,
-});
-
-skillsObserver.observe(skillsContainer);
-
-// Exp card animation
-const revealExp = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove('push-right');
-  observer.unobserve(entry.target);
-};
-
-const expObserver = new IntersectionObserver(revealExp, {
-  root: null,
-  threshold: 0.6,
-});
-
-expCard.forEach(card => {
-  expObserver.observe(card);
-});
-
-// Education card animation
-const revealEdu = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove('push-left');
-  observer.unobserve(entry.target);
-};
-
-const eduObserver = new IntersectionObserver(revealEdu, {
+const sectionObserver = new IntersectionObserver(revealSections, {
   root: null,
   threshold: 0.25,
 });
 
+sectionObserver.observe(skillsContainer);
+
+expCard.forEach(card => {
+  sectionObserver.observe(card);
+});
+
 educationCard.forEach(card => {
-  eduObserver.observe(card);
+  sectionObserver.observe(card);
 });
