cc986b10ac1c384b3ea09572ae37759cf290ef4b
Edrickasper <edrickasper66@gmail.com>
2025-01-26 19:54:47 +0530

    Back to top btn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const heroContainer = document.querySelector('.container');
 const skillsContainer = document.querySelector('.skills-container');
 const expCard = document.querySelectorAll('.timeline-card');
 const educationCard = document.querySelectorAll('.education-card');
+const arrowBtn = document.getElementById('arrow-btn');
 
 window.addEventListener('load', () => {
   spinner.classList.add('hidden');
   heroContainer.classList.remove('hidden');
 });
 document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();
   // Add your form submission logic here
   // You can use fetch API or any other method to send the form data
   console.log('Form submitted!');
@@ -39,3 +39,17 @@ expCard.forEach(card => {
 educationCard.forEach(card => {
   sectionObserver.observe(card);
 });
+
+const showArrowBtn = new IntersectionObserver((entries) => {
+  const [entry] = entries;
+  if(entry.isIntersecting) {
+    arrowBtn.classList.add('fadein')
+  } else {
+    arrowBtn.classList.remove('fadein')
+  };
+}, {
+  root: null,
+  threshold: 0.25
+})
+
+showArrowBtn.observe(heroContainer)
